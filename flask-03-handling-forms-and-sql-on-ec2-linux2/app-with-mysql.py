a816e37f2682a825f77e39effa06b0d380fa66ed
Fatih <fatihozguner@hotmail.com>
2020-09-16 21:13:50 -0500

    updated code

--- flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-mysql.py
+++ flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-mysql.py
@@ -63,6 +63,32 @@ def find_emails(keyword):
         user_emails = [('Not found.', 'Not Found.')]
     return user_emails
 
+# Write a function named `insert_email` which adds new email to users table the db.
+
+def insert_email(name, email):
+    query = f"""
+    SELECT * FROM users WHERE username like '{name}';
+    """
+    cursor.execute(query)
+    result = cursor.fetchall()
+    # default text
+    response = 'Error occurred..'
+    # if user input are None (null) give warning
+    if name == None or email == None:
+        response = 'Username or email can not be emtpy!!'
+    # if there is no same user name in the db, then insert the new one
+    elif not any(result):
+        insert = f"""
+        INSERT INTO users
+        VALUES ('{name}', '{email}');
+        """
+        cursor.execute(insert)
+        response = f'User {name} added successfully'
+    # if there is user with same name, then give warning
+    else:
+        response = f'User {name} already exits.'
+    return response
+
 # Write a function named `emails` which finds email addresses by keyword using `GET` and `POST` methods,
 # using template files named `emails.html` given under `templates` folder
 # and assign to the static route of ('/')
@@ -75,6 +101,22 @@ def emails():
         return render_template('emails.html', name_emails=user_emails, keyword=user_name, show_result=True)
     else:
         return render_template('emails.html', show_result=False)
+
+# Write a function named `add_email` which inserts new email to the database using `GET` and `POST` methods,
+# using template files named `add-email.html` given under `templates` folder
+# and assign to the static route of ('add')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_email():
+    if request.method == 'POST':
+        user_name = request.form['username']
+        user_email = request.form['useremail']
+        result = insert_email(user_name, user_email)
+        return render_template('add-email.html', result=result, show_result=True)
+    else:
+        return render_template('add-email.html', show_result=False)
+
+# Add a statement to run the Flask application which can be reached from any host on port 80.
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
